66c1bb3dd79099829015a7c4f4d01a285e233a8a
Pixii <pixiibomb@gmail.com>
2023-09-25 11:26:39 -0500

    git tag -a v1.0.13 -m "Release version 1.0.13"

--- publish/config/pixii.php
+++ publish/config/pixii.php
@@ -7,14 +7,14 @@ return [
     | PixiiBomb Essentials Configuration
     |--------------------------------------------------------------------------
     |
-    | This option controls the default authentication "guard" and password
-    | reset options for your application. You may change these defaults
-    | as required, but they're a perfect start for most applications.
+    | Refer to the documentation on github.
     |
     */
 
     CONFIG => [
-        INCLUDE_SITE_NAME_IN_TITLE => true
+        LAYOUT_APP => 'layouts.app',
+        INCLUDE_SITE_NAME_IN_TITLE => true,
+        DEFAULT_NAVBAR_VIEW => NAVBAR
     ],
 
     META => [
