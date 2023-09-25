c6cc68299b2acd63ffef3bc5e966063d7f419279
Pixii <pixiibomb@gmail.com>
2023-09-25 10:51:20 -0500

    Added Footer to Published Files

---
+++ publish/resources/views/includes/footer.blade.php
@@ -0,0 +1,15 @@
+<div class="container-fluid">
+    <div class="row">
+        <div class="col" id="Brand">
+            logo
+        </div>
+        <div class="col" id="Contact">
+            <div>
+                <button class="btn btn-icon">in</button>
+                <button class="btn btn-icon">f</button>
+                <button class="btn btn-icon">gram</button>
+            </div>
+            <p>Copyright pixiibomb 2023. All Rights Reserved.</p>
+        </div>
+    </div>
+</div>
